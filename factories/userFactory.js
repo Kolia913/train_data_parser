b0641dcecd3bf9c5dada949264f6cc4bc62ac393
Kolia913 <mynew1mail470@gmail.com>
2024-04-13 05:15:15 +0300

    feat: added users factory

--- factories/userFactory.js
+++ factories/userFactory.js
@@ -1,7 +1,35 @@
 const { pgClient } = require("../dbClient");
 const { faker } = require("@faker-js/faker");
+const { pbkdf2: _pbkdf2, randomBytes } = require("node:crypto");
+const { promisify } = require("node:util");
 
-async function makeUsers(count) {}
+const pbkdf2 = promisify(_pbkdf2);
+
+async function makeUsers(count) {
+  let sqlQuery = `INSERT INTO "user"(phone, email, password, name) VALUES `;
+  for (let i = 0; i < count; i++) {
+    const phone = faker.phone.number();
+    const email = faker.internet.email();
+    const name = faker.person.fullName();
+    const password = await getSaltedPasswordHash("password");
+    sqlQuery += `('${phone}', '${email}', '${password}', '${name}'),`;
+  }
+  sqlQuery = sqlQuery.trim().slice(0, -1);
+  sqlQuery += ";";
+  const res = await pgClient.query(sqlQuery);
+  console.log("res - ", res);
+}
+
+async function getSaltedPasswordHash(password) {
+  const salt = randomBytes(8).toString("hex");
+  const hash = await getPasswordHash(password, salt);
+  return salt + "." + hash.toString("hex");
+}
+
+async function getPasswordHash(password, salt) {
+  const hash = await pbkdf2(password, salt, 10000, 32, "sha512");
+  return hash;
+}
 
 module.exports = {
   makeUsers,
--- main.js
+++ main.js
@@ -4,19 +4,21 @@ const { pgClient } = require("./dbClient");
 const { getStops } = require("./parsers/stationsParser");
 const { getSegments } = require("./parsers/trainsRoutesParser");
 const { generateFaresQuery } = require("./generators/faresGenerator");
+const { makeUsers } = require("./factories/userFactory");
 
 pgClient
   .connect()
   .then(() => {
-    getSegments()
-      .then(() => {
-        process.exit(0);
-      })
-      .catch((err) => {
-        console.error(err);
-        process.exit(1);
-      });
+    // getSegments()
+    //   .then(() => {
+    //     process.exit(0);
+    //   })
+    //   .catch((err) => {
+    //     console.error(err);
+    //     process.exit(1);
+    //   });
     // pgClient.query(generateFaresQuery());
+    makeUsers(100);
   })
   .catch((error) => {
     console.error(error);
